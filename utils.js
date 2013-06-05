936f9168f4af17266cff16413796f97577d55d40
Guillaume Charmetant <cguille.dev@gmail.com>
2013-06-05 18:03:06 +0300

    Update utils.js
    Do not return `null` for falsy but defined values

--- utils.js
+++ utils.js
@@ -1,8 +1,7 @@
 // Creating accessors which automatically parse JSON data
-// Is there a way to use it as : localStorage.parser.getItem("item") ?
 
 Storage.prototype.parseAndGetItem = function (key) {
-    return this[key] ? JSON.parse(this[key]) : null;
+    return typeof(this[key]) !== 'undefined' ? JSON.parse(this[key]) : null;
 };
 Storage.prototype.stringifyAndSetItem = function (key, data) {
     this[key] = JSON.stringify(data);
